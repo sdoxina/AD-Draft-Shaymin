5c358c45e7bc2644304bdd578304a0fa17c1fe0d
sdoxina <202311026@fit.edu.ph>
2025-07-06 07:46:34 +0800

    feat(landing): add initial HTML structure and include necessary components

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+if (!defined('BASE_PATH')) {
+    define('BASE_PATH', __DIR__);
+}
+require_once BASE_PATH . '/vendor/autoload.php';
+require_once BASE_PATH . '/utils/htmlEscape.util.php';
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Outlast</title>
+    <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="icon" type="image/x-icon" href="./assets/img/outlastFavicon.png">
+</head>
+<body>
+    <!-- Nav -->
+     <?php require_once BASE_PATH . '/components/templates/nav.component.php'; ?>
+
+    <section class="hero-section">
+         <div class="container-fluid">
+         <div id="image"></div>
+         <div id="glitch"></div>
+      </div>
+      </section>
+
+      <section class="about-section">
+         <div class="container-fluid">
+
+         </div>
+      </section>
+
+      <!-- Footer -->
+       <?php require_once BASE_PATH . '/components/templates/footer.component.php'; ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+      <script src="./assets/js/script.js"></script>
+</body>
+</html>
