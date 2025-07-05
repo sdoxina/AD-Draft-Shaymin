11b43b4b2c5c3962a38ac030035506614511b731
sdoxina <202311026@fit.edu.ph>
2025-07-06 07:48:25 +0800

    feat: create pageData handler for navigation and footer links

---
+++ handlers/pageData.handler.php
@@ -0,0 +1,23 @@
+<?php
+// handlers/pageData.handler.php
+
+$navList = [
+    ["label" => "Home", "url" => "/index.php"],
+    ["label" => "Items", "url" => "/pages/ExamplePage/index.php"],
+    ["label" => "Login", "url" => "/pages/login-signup/index.php"]
+];
+
+// For Footer
+$footerLinks = [
+    ["label" => "Home", "url" => "/index.php"],
+    ["label" => "Items", "url" => "/pages/ExamplePage/index.php"]
+];
+
+$webDesigner = [
+    "name" => "Shaymin",
+    "role" => "Web Designer"
+];
+
+$companyLogo = "/assets/img/outlastLogoRed.png";
+$companyCopyright = "© 2025 Outlast. All rights reserved.";
+?>
